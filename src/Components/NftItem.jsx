ea29b0350c4b6af998c7003d741652f6038c4cb6
BlueBigThink <bluebigthink@gmail.com>
2023-03-29 06:24:00 +0300

    add disabled

--- src/Components/NftItem.jsx
+++ src/Components/NftItem.jsx
@@ -5,6 +5,7 @@ import Info from '../Assets/info.png';
 
 const NftItem = (props) => {
   const {item} = props;
+
   return (
     <>
       <div className="group w-[20rem] flex flex-col">
@@ -13,17 +14,16 @@ const NftItem = (props) => {
             <img class="z-[1] right-0 top-0 absolute" alt="" src={TrBorder}/>
             <img class="z-[2] right-2 top-2 absolute" alt="" src={Info}/>
             <img class="z-[1] left-0 bottom-0 absolute" alt="" src={BlBorder}/>
-            {/* <div className="bg-coin-100 bg-cover h-40 w-40 group-hover:hidden"></div> */}
-            <div className="bg-coin-100 bg-cover h-40 w-40"></div>
+            <div className={`bg-coin-${item.coin} bg-cover h-40 w-40`}></div>
           </div>
         </div>
         <div className="flex-1 flex -translate-y-10 bg-gradient-to-b from-[#000533] to-[#0B50A0] p-[2px] group-hover:from-[#00E0FF] group-hover:to-[#00E0FF] w-full">
         <div className="flex-1 bg-gradient-to-b to-[#000533] from-[#070F56] py-10 w-full flex flex-col items-center gap-6">
           <p className="uppercase py-6 font-bold text-2xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-t from-[#A5F4FF] to-[#3250F0]">
-            gold
+            {item.name}
           </p>
           <div className="cursor-pointer cut-edge-button w-48 p-[2px] drop-shadow-2xl bg-[#0026F4] bg-gradient-to-t from-[#000000]/80 via-[#000000]/50 to-[#0026F4] group-hover:from-[#00E0FF] group-hover:via-[#00E0FF] group-hover:to-[#00E0FF]">
-            <button disabled="" className="cut-edge-button w-full py-2 bg-gradient-to-b from-[#0026F4]/0 via-[#089DB1]/50 to-[#089DB1] disabled:from-gray-900 disabled:via-gray-700 disabled:to-gray-500">
+            <button disabled={`${item.disabled ? '' : 'true'}`} className="cut-edge-button w-full py-2 bg-gradient-to-b from-[#0026F4]/0 via-[#089DB1]/50 to-[#089DB1] disabled:from-gray-900 disabled:via-gray-700 disabled:to-gray-500">
               <p className="text-white font-bold text-3xl text-center">
                 BUY
               </p>
--- src/Pages/NFT.jsx
+++ src/Pages/NFT.jsx
@@ -17,27 +17,39 @@ import Coin5000 from '../Assets/5000.png';
 const nftlist = [
   {
     name: 'BRONZE',
+    coin : 100,
     img: Coin100,
+    disabled : true,
   },
   {
     name: 'SILVER',
+    coin : 200,
     img: Coin200,
+    disabled : true,
   },
   {
     name: 'PEARL',
+    coin : 500,
     img: Coin500,
+    disabled : false,
   },
   {
     name: 'RUBY',
+    coin : 1000,
     img: Coin1000,
+    disabled : false,
   },
   {
     name: 'GOLD',
+    coin : 3000,
     img: Coin3000,
+    disabled : false,
   },
   {
     name: 'DIAMOND',
+    coin : 5000,
     img: Coin5000,
+    disabled : false,
   },
 ];
 function NFT() {
@@ -46,11 +58,11 @@ function NFT() {
       <TopBar selected={1}/>
       <Box sx={{color:'white'}}>
         <Container fixed sx={{padding : "20px 0"}}>
-          <Grid container spacing={3}>
+          <Grid container spacing={3} sx={{margin : 0, padding : 0}}>
               {
                 nftlist.map((nft, index) => {
                   return(
-                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
                       <NftItem item={nft}/>
                     </Grid>
                   )
