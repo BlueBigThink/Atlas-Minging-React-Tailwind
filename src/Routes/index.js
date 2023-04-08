b4f044ca42d4c5d7ea5be113cc6ed990b115ce94
BlueBigThink <bluebigthink@gmail.com>
2023-04-07 21:35:03 -0400

    add routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,32 @@ import React from 'react'
 import { useRoutes } from 'react-router-dom'
 
 import Home from '../Pages/Home';
+import NFT from '../Pages/NFT';
+import Search from '../Pages/Search';
 
 const mainRoutes = [
   {
     path: '/',
     element: <Home />,
-  }
+  },
+  {
+    path: '/nft',
+    element: <NFT />,
+    children: [
+      {
+        index: true,
+        element: <NFT />,
+      },
+      {
+        path: '/nft/detail/:id',
+        element: <div>NFT Detail</div>,
+      },
+    ],
+  },
+  {
+    path: '/search',
+    element: <Search />,
+  },
 ];
 
 const AppRoutes = () => {
